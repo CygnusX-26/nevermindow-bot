c49e4451924ac0dca7d607dc1224e2ef5bbf0baf
CygnusX-26 <qh4nveh@gmail.com>
2023-12-10 00:50:13 -0800

    add funny

--- bot/cogs/bot.py
+++ bot/cogs/bot.py
@@ -1,4 +1,5 @@
-import sqlite3
+from io import BytesIO
+import os
 from aiohttp import ClientSession
 import discord
 from discord.ext import commands
@@ -6,6 +7,8 @@ from discord import app_commands
 import asyncio
 import datetime
 import random
+from PIL import Image
+import requests
 
 name = 'Wyvern'
 tag = '11208'
@@ -30,6 +33,26 @@ class Bot(commands.Cog):
         if (message.author.id == 340590217658630144):
             if (random.randint(0, 1000) == 420):
                 await message.channel.send("YOU ARE TERRIBLE NEBERMINDOW")
+        if (message.author.bot == False and random.randint(0, 10000) == 420):
+            base_image = Image.open('big_thumb.png')
+            response = requests.get(message.author.display_avatar)
+            if response.status_code == 200:
+                try:
+                    overlay_image = Image.open(BytesIO(response.content))
+                    overlay_image = overlay_image.resize((125,125))
+                    base_image.paste(overlay_image, (25,35))
+                    base_image.paste(overlay_image, (25,300))
+                    base_image.paste(overlay_image, (25,580))
+                    base_image.save('./bot/cogs/temp/output.png')
+                    await message.reply(file=discord.File('./bot/cogs/temp/output.png'))
+                    os.remove('./bot/cogs/temp/output.png')
+                except Exception as e:
+                    print(e)
+                    await message.channel.send("Failed, please ping cygnus this should not have happened...")
+            else:
+                print("Failed to download the image")
+                await message.channel.send("Failed, please ping cygnus this should not have happened...")
+            
 
     #what gets run when the bot starts
     @commands.Cog.listener()
