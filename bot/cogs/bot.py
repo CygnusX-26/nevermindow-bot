25dbbbb5fd13d9026389860d6c41c3d510ae3e95
CygnusX-26 <qh4nveh@gmail.com>
2024-03-16 21:01:14 -0700

    gaming friday updates

--- bot/cogs/bot.py
+++ bot/cogs/bot.py
@@ -120,6 +120,34 @@ class Bot(commands.Cog):
     async def mutenevermindmobile(self, interaction: discord.Interaction) -> None:
         self.mutewolfymobile = not(self.mutewolfymobile)
         await interaction.response.send_message(f"Set nevermind mobile blocking to {self.mutewolfymobile}", ephemeral=True)
+
+    @app_commands.command(name='gamingfriday', description='gets the amount of time till next gaming friday')
+    async def gamingfriday(self, interaction: discord.Interaction) -> None:
+        await interaction.response.defer()
+        events = await interaction.guild.fetch_scheduled_events()
+        start_time = None
+        for event in events:
+            if "friday" in event.name.lower():
+                start_time = event.start_time
+                break
+        if not start_time:
+            await interaction.followup.send("No gaming friday found", ephemeral=True)
+            return
+        informal_time = f'<t:{int(start_time.timestamp())}:R>'
+        formal_time = f'<t:{int(start_time.timestamp())}:F>'
+        description = f"""The next **GAMING FRIDAY** will take place on {formal_time}\n> Which is {informal_time} see you soon!\n"""
+        embed = discord.Embed(
+            title = f":exclamation: GAMING FRIDAY :exclamation:",
+            description = description,
+            color = discord.Color.from_str("#2F3136")
+        )
+        user_list = ''
+        async for user in event.users():
+            user_list += f"{user.mention}\n"
+        if user_list == '':
+            user_list = "No one yet :("
+        embed.add_field(name="Current list of people attending: ", value=user_list, inline=True)
+        await interaction.followup.send(embed=embed, ephemeral=True)
     
 
 
